3fd3137fae627de4af74562d416376e339fafb61
yainvi61 <yainvi.2002@gmail.com>
2025-01-29 19:23:28 -0600

    Update exercises_01.py

--- py/exercises_01.py
+++ py/exercises_01.py
@@ -5,27 +5,29 @@
 # orden apellido paterno, apellido materno y nombres, ejemplo: Revilla Silva Jesús Alberto
 # comienza cada nombre con mayúscuala y el resto con minúsculas incluye acentos y otros
 # caracteres del Español.
-# Escribe el código aquí
+name="Villanueva Infante Jorge Yahir"
 
 
 
 # 1.1 Declara una variable matricula y asigna tu número de matrícula como entero
-# Escribe el código aquí
-
+matricula=175367
 
 
 # 2. Crear una función que sume dos números
-def sumar(a, b):
-    pass # reemplaza pass con tu código
+def sumar(a,b):
+    return a+b
+sumar(1,3)
 
 
 # 3. Usar un bucle for para imprimir los números del 1 al 10
 # reemplaza la palabra reservada pass con tu código
 def bucle_for():
-    pass
-
+    for i in range(10):
+        print(i+1)
+bucle_for()
 
 # 5. Usar un bucle para imprimir cada número
 def print_numbers():
     numbers = [1, 2, 3, 4, 5]
-    # Escribe el código aquí
+    for number in numbers:
+        print(number)
